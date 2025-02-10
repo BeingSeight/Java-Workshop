c7f03e15875a61324f671054b71a005d86db50a1
@BeingSeight <ss9656484@gmail.com>
2025-02-10 20:28:04 +0530

    java workshop day1

---
+++ bStudent.java
@@ -0,0 +1,10 @@
+// student details id name age
+public class bStudent {
+
+    public static void main(String[] args) {
+        System.out.println("Student Details");
+        System.out.println("ID: 1");
+        System.out.println("Name: John Doe");
+        System.out.println("Age: 20");
+    }
+}
---
+++ dJackedArr.java
@@ -0,0 +1,16 @@
+// Jacked array a1[0] a1[1] a1[2] new int from 2 to 4
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class dJackedArr {
+
+    public static void main(String[] args) {
+        int[][] a1 = new int[3][];
+        a1[0] = new int[2];
+        a1[1] = new int[3];
+        a1[2] = new int[4];
+
+        Scanner sc = new Scanner(System.in);
+ 
+    sc.close();
+}
